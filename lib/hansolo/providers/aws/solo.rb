7e7b5f47110224ade371b8af7e940ea0b91adc05
Eduardo Gutierrez <eduardo@vermonster.com>
2013-07-30 13:44:56 -0400

    Address intermittent S3 error
    The s3 bucket is being shared between threads and is lazily initialized,
    potentially leading to one thread accessing it while it is still being
    initialized.
    
    Prepare the command to run before starting any threads instead of each
    time a host connection is established.

--- lib/hansolo/providers/aws/solo.rb
+++ lib/hansolo/providers/aws/solo.rb
@@ -1,28 +1,27 @@
 module Hansolo::Providers::AWS
   module Solo
     def sync_data_bags
+      items = bucket.objects.with_prefix(Hansolo.app)
+
+      command = items.inject([]) do |cmd, object|
+        key = object.key.sub("#{Hansolo.app}/", '')
+        path = Pathname.new('/tmp/data_bags').join(key)
+
+        cmd << "mkdir -p #{path.dirname}"
+        cmd << "echo '#{object.read}' > #{path}"
+      end
+
+      command = command.join('; ')
+
       threads = hosts.map do |host|
         Thread.new do
           ssh = connect(host)
-
-          command = data_bag_items.inject([]) do |cmd, object|
-            key = object.key.sub("#{Hansolo.app}/", '')
-            path = Pathname.new('/tmp/data_bags').join(key)
-
-            cmd << "mkdir -p #{path.dirname}"
-            cmd << "echo '#{object.read}' > #{path}"
-          end
-
-          ssh.exec! command.join('; ')
+          ssh.exec! command
           ssh.close
         end
       end
 
       threads.map(&:join)
-    end
-
-    def data_bag_items
-      bucket.objects.with_prefix(Hansolo.app)
     end
   end
 end
