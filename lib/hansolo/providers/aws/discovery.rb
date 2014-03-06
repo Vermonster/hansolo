dfa44ed9a0f5c2c47c0d0232d5e16981d9a6ba8c
Eduardo Gutierrez <eduardo@vermonster.com>
2014-03-06 13:33:17 -0500

    Allow custom URI syntax for AWS target discovery

--- lib/hansolo/providers/aws/discovery.rb
+++ lib/hansolo/providers/aws/discovery.rb
@@ -16,23 +16,37 @@ module Hansolo
 
             return uri if uri.scheme == 'ssh'
 
-            instance = instances_by_tag(uri.scheme.to_s, uri.host).first
-            raise ArgumentError, "no gateway with #{uri.scheme} #{uri.host} found" if instance.nil?
+            ip_address = instance_ip_addresses_by_tag(uri.scheme.to_s, uri.host).first
+            raise ArgumentError, "no gateway with #{uri.scheme} #{uri.host} found" if ip_address.nil?
 
-            URI.parse("ssh://#{uri.user}@#{instance.public_ip_address}:#{uri.port || 22}")
+            URI.parse("ssh://#{uri.user}@#{ip_address}:#{uri.port || 22}")
           end
         end
 
         def hosts
           @hosts ||= begin
             target = Hansolo.target
-            return super unless target.is_a?(Hash)
 
-            target_instances = instances_by_tag(target[:host].to_s, Hansolo.app)
+            if target.is_a?(Hash)
+              target_instances = instance_ip_addresses_by_tag(target[:host].to_s, Hansolo.app)
 
-            target_instances.map do |instance|
-              ip_address = instance.ip_address || instance.private_ip_address
-              URI.parse("ssh://#{target[:user]}@#{ip_address}:#{target[:port] || 22}")
+              target_instances.map do |ip_address|
+                URI.parse("ssh://#{target[:user]}@#{ip_address}:#{target[:port] || 22}")
+              end
+            else
+              target.inject([]) do |uris, uri|
+                uri = URI.parse(uri)
+
+                if uri.scheme == 'ssh'
+                  uris << uri
+                else
+                  ip_addresses = instance_ip_addresses_by_tag(uri.scheme.to_s, uri.host)
+
+                  uris += ip_addresses.map do |ip_address|
+                    URI.parse("ssh://#{uri.user}@#{ip_address}:#{uri.port || 22}")
+                  end
+                end
+              end
             end
           end
         end
@@ -49,10 +63,10 @@ module Hansolo
           end
         end
 
-        def instances_by_tag(tag, value)
-          ec2.instances.tagged(tag).tagged_values(value).select do |instance|
-            instance.status != :terminated
-          end
+        def instance_ip_addresses_by_tag(tag, value)
+          instances = ec2.instances.tagged(tag).tagged_values(value).to_a
+          instances.select! { |instance| instance.status != :terminated }
+          instances.map! { |instance| instance.ip_address || instance.private_ip_address }
         end
       end
     end
